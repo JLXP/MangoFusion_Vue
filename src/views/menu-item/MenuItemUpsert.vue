3835c8940ec288bedf76c5c695851ef2326b4fff
JavierXool <javier.xool@outlook.com>
2026-01-21 09:58:06 -0600

    modifications in flow

--- src/views/menu-item/MenuItemUpsert.vue
+++ src/views/menu-item/MenuItemUpsert.vue
@@ -14,8 +14,9 @@
             type="submit"
             form="menuForm"
             class="btn btn-success btn-sm gap-2 rounded-1 px-4 py-2"
+            :disabled="isProcessing"
           >
-            <span class="spinner-border spinner-border-sm me-2"></span>
+            <span v-if="isProcessing" class="spinner-border spinner-border-sm me-2"></span>
             {{ menuItemIdForUpdate ? 'Update' : 'Create' }} Item
           </button>
 
@@ -232,6 +233,7 @@ const onFormSubmit = async (e) => {
         .createMenuItem(formData)
         .then(() => {
           alert('menu item created')
+          router.push({ name: APP_ROUTE_NAMES.MENU_ITEM_LIST })
         })
         .catch((err) => {
           isProcessing.value = false
@@ -243,6 +245,7 @@ const onFormSubmit = async (e) => {
         .updateMenuItem(menuItemIdForUpdate, formData)
         .then(() => {
           alert('menu item updated')
+          router.push({ name: APP_ROUTE_NAMES.MENU_ITEM_LIST })
         })
         .catch((err) => {
           isProcessing.value = false
