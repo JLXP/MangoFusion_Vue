b0f3b7a4d4605b36b8bacd15d3b7a48831bb3d47
JavierXool <javier.xool@outlook.com>
2026-02-18 23:56:39 -0600

    sign up in auth service

---
+++ src/services/authService.js
@@ -0,0 +1,26 @@
+import api from '@/services/api'
+
+export default {
+  async signUp(userData) {
+    try {
+      const response = await api.post('/auth/register', {
+        email: userData.email,
+        password: userData.password,
+        name: userData.name,
+        role: userData.role,
+      })
+      console.log('Sign Up Response:', response)
+      if (response.data.isSuccess) {
+        return {
+          success: true,
+          message: 'User registered successfully',
+        }
+      } else {
+        throw new Error('Registraton failed')
+      }
+    } catch (error) {
+      console.error('Error in Registration', error)
+      throw error
+    }
+  },
+}
