65edf3de37cb6d5dcb8fa4a3c25f346c92031a1b
JavierXool <javier.xool@outlook.com>
2026-02-27 11:11:32 -0600

    initialize authentication

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import router from './router/routes'
 
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { useThemeStore } from './stores/themeStore'
+import { useAuthStore } from './stores/authStore'
 
 const app = createApp(App)
 const pinia = createPinia()
@@ -22,5 +23,8 @@ const themeStore = useThemeStore()
 if (themeStore.theme) {
   document.body.setAttribute('data-bs-theme', themeStore.theme)
 }
+
+const authStore = useAuthStore()
+authStore.initialize()
 
 app.mount('#app')
--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -13,7 +13,6 @@ export const useAuthStore = defineStore('authStore', () => {
     password: '',
     name: '',
     id: '',
-    isLoggedIn: false,
   })
 
   const isAuthenticated = ref(false)
@@ -22,6 +21,40 @@ export const useAuthStore = defineStore('authStore', () => {
   const getUserInfo = computed(() => {
     return isAuthenticated.value ? user : null
   })
+
+  function decodeToken(token) {
+    const payload = JSON.parse(atob(token.split('.')[1]))
+    return {
+      email: payload.email,
+      name: payload.name,
+      id: payload.id,
+      role: payload.role,
+    }
+  }
+
+  function initialize() {
+    try {
+      //Se obtiene el token
+      const token = Cookies.get('token_mango')
+
+      if (token) {
+        //Se decodifica el token para obtener la información del usuario
+        const userData = decodeToken(token)
+        if (userData) {
+          Object.assign(user, userData)
+          isAuthenticated.value = true
+        } else {
+          clearAuthData()
+        }
+      } else {
+        clearAuthData()
+      }
+    } catch (err) {
+      console.error('Error initializing auth store:', err)
+      clearAuthData()
+    }
+  }
+
   //actions
   async function signUp(userData) {
     try {
@@ -42,13 +75,11 @@ export const useAuthStore = defineStore('authStore', () => {
       //async await
       const { token, user: userData } = await authService.signIn(formObj)
       Object.assign(user, userData)
-      user.isLoggedIn = true
       isAuthenticated.value = true
       Cookies.set('token_mango', token, { expires: 7 })
-
       router.push('/')
 
-      //const { showSuccess } = useSwal()
+      //const { showSuccess } = useSwal()s
       //showSuccess('Login successful')
       //router.push(APP_ROUTE_NAMES.HOME)
     } catch (err) {
@@ -59,11 +90,23 @@ export const useAuthStore = defineStore('authStore', () => {
     }
   }
 
+  function clearAuthData() {
+    Object.assign(user, {
+      email: '',
+      password: '',
+      name: '',
+      id: '',
+    })
+    isAuthenticated.value = false
+    Cookies.remove('token_mango')
+  }
+
   return {
     user,
     isAuthenticated,
     getUserInfo,
     signUp,
     signIn,
+    initialize,
   }
 })
